6147058a2a43c6999d2e77611e2b003cc356831d
Mohit Kumar <Mohit Kumar@Simranmohit>
2024-04-13 13:33:42 +0530

    add code

---
+++ EMSProjects/src/main/java/com/emsProjects/EmsProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.emsProjects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsProjectsApplication.class, args);
+	}
+
+}
---
+++ EMSProjects/src/test/java/com/emsProjects/EmsProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emsProjects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
